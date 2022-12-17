fb3bd407e703aca5157e8d3e70e555e53ac3c047
Parth Tyagi <parthtyagicode@gmail.com>
2022-12-17 19:19:04 +0530

    adding modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Main from './components/main';
+import Modal from './components/modal';
 
 function App() {
   return (
     <>
       <Navbar />
       <Main />
+      <Modal />
     </>
   );
 }
---
+++ frontend/src/components/modal.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { GrClose } from 'react-icons/gr';
+import './modal.css';
+
+export default function Modal() {
+    return (
+        <div className='modal-container'>
+            <div className="modal-header">
+                <h3>Munna Bhai MBBS</h3>
+                <button><GrClose /></button>
+            </div>
+
+            <div className="modal-body">
+                <div className="modal-body-img">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1VejMer5gFLAaM7l-1dMKqwJeZYJp9O2OlwqJ4P0-n1nnjtpb" alt="movieImage" />
+                </div>
+                <div className="movie-info">
+                    <p><span>Release date:</span> May 13, 2021</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia labore odio ab quisquam atque sequi non dolore officiis modi alias cupiditate quidem maxime, culpa harum hic rerum impedit temporibus.</p>
+                    <p><span>6.6</span> / 10 (1214 total votes)</p>
+                </div>
+            </div>
+        </div>
+    )
+}
